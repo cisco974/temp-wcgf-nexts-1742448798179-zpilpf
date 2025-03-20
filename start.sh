9546e88fa11796c23dec5c70a9213d8400132e8c
cisco974 <vitryfrancois@gmail.com>
2025-03-20 09:46:28 +0400

    Copy start.sh from wcgf-nexts

---
+++ start.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+source /home/cayi7350/nodevenv/test.wcgf.com/22/bin/activate
+cd /home/cayi7350/test.wcgf.com || exit 1
+LOG_FILE="/home/cayi7350/test.wcgf.com/nextjs.log"
+
+echo "[$(date)] Démarrage de l'application Next Js : test-wcgf avec PM2..." | tee -a "$LOG_FILE"
+
+cd /home/cayi7350/test.wcgf.com || exit 1
+
+# Vérifier si l'application est déjà en cours d'exécution sous PM2
+if npx pm2 list | grep -q "test-wcgf"; then
+    echo "[$(date)] L'application est déjà en cours d'exécution sous PM2." | tee -a "$LOG_FILE"
+    npx pm2 stop test-wcgf
+    npx pm2 delete test-wcgf
+fi
+
+# Activer l'environnement Node.js
+source /home/cayi7350/nodevenv/test.wcgf.com/22/bin/activate
+
+# Démarrer l'application avec PM2
+npx pm2 start npm --name "test-wcgf" --watch --interpreter bash -- run start
+
+# Sauvegarder la configuration PM2
+npx  pm2 save --force
+
+
+
+echo "[$(date)] L'application Next Js : test-wcgf  a été démarrée avec PM2." | tee -a "$LOG_FILE"
